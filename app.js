59fbe4b7227fa5c78b06655d28f32c93fd949d4b
yaerhee <yaerhee@naver.com>
2022-10-01 23:59:01 +0900

    form tag apply & preventDefault practice

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const loginForm = document.getElementById("login-form")
+// search element among login-form (directly)
+const loginForm = document.querySelector('#login-form') // form 으로 직접 변경
+const loginInput = document.querySelector('#login-form input')
 
+const onLoginSubmit = e => {
+    e.preventDefault()
+    console.log("hello", loginInput.value)
+}
+
+loginForm.addEventListener('submit', onLoginSubmit)
