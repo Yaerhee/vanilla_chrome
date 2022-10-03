3b5b254e634d6f9cf8c00ad3c02581ee2018f6be
yaerhee <yaerhee@naver.com>
2022-10-03 23:52:06 +0900

    flexible css(hidden) with classList

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 // search element among login-form (directly)
 const loginForm = document.querySelector('#login-form') // form 으로 직접 변경
 const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
 
-const onLoginSubmit = e => {
+const HIDDEN_CLASSNAME = "hidden" // variable with only string(s)
+
+const onLoginSubmit = e => { // submit event
     e.preventDefault()
-    console.log("hello", loginInput.value)
+    const userName = loginInput.value
+    loginForm.classList.add('hidden') // hide input (userName exists)
+    greeting.innerText = `Hello ${userName}` // = "Hello" + userName
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener('submit', onLoginSubmit)
