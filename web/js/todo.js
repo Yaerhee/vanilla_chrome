be6655cb3af1e0766e5c67c072633890541140fb
yaerhee <yaerhee@naver.com>
2022-10-13 00:53:15 +0900

    JSON.stringify, JSON.parse + arg 생략하여 실행하는 함수

--- web/js/todo.js
+++ web/js/todo.js
@@ -2,10 +2,13 @@ const toDoForm = document.getElementById('todo-form')
 const toDoInput = toDoForm.querySelector('#todo-form input')
 const toDoList = document.getElementById('todo-list')
 
+const TODOS_KEY = "todos"
 const toDos = []
 
-const saveToDo = toDos => {
-    localStorage.setItem("todos", toDos)
+const saveToDo = () => {
+    // array 타입으로 값을 저장하기 위해 JSON.stringify 처리
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+    // JSON.stringify <=> JSON.parse 로 array <=> string
 }
 
 const deleteToDo = e => {
@@ -35,6 +38,14 @@ const handleToDoSubmit = e => {
     toDoInput.value = ''
     toDos.push(newToDo)
     paintToDo(newToDo)
+    saveToDo()
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit)
+
+const savedToDos = localStorage.getItem(TODOS_KEY) // string when console
+
+if (savedToDos) {
+    const parsedToDos = JSON.parse(savedToDos)
+    parsedToDos.forEach(paintToDo) // newToDo 에 item 이 자동으로 들어가므로 언급할 필요 없음(!)
+}
