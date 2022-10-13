6fbf45c2013f1eb99d10eac963799a9f34304e59
yaerhee <yaerhee@naver.com>
2022-10-13 23:40:25 +0900

    delete(exclude & reset Array) from localStorage

--- web/js/todo.js
+++ web/js/todo.js
@@ -3,7 +3,8 @@ const toDoInput = toDoForm.querySelector('#todo-form input')
 const toDoList = document.getElementById('todo-list')
 
 const TODOS_KEY = "todos"
-const toDos = []
+let toDos = []
+// page reset -> array reset ? 이 아니라 localStorage 에 있던 값이 그대로 다시 나오도록 하기
 
 const saveToDo = () => {
     // array 타입으로 값을 저장하기 위해 JSON.stringify 처리
@@ -12,15 +13,18 @@ const saveToDo = () => {
 }
 
 const deleteToDo = e => {
-    const targetLi = e.target.parentElement
-    targetLi.remove()
+    const li = e.target.parentElement
+    li.remove() // delete clicked li
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id)) // exclude clicked id
+    // toDos 를 filter 한 Object 로 갱신한 후, localStorage 에 다시 setItem
+    saveToDo()
 }
 
-const paintToDo = newToDo => {
+const paintToDo = newToDoObj => {
     const li = document.createElement('li')
-
+    li.id = newToDoObj.id
     const span = document.createElement('span')
-    span.innerText = newToDo
+    span.innerText = newToDoObj.text
 
     const btn = document.createElement('button')
     btn.innerText = '❌'
@@ -36,8 +40,13 @@ const handleToDoSubmit = e => {
     e.preventDefault()
     const newToDo = toDoInput.value
     toDoInput.value = ''
-    toDos.push(newToDo)
-    paintToDo(newToDo)
+    const newToDoObj = {
+        text: newToDo,
+        id: Date.now()
+    }
+
+    toDos.push(newToDoObj)
+    paintToDo(newToDoObj)
     saveToDo()
 }
 
@@ -47,5 +56,8 @@ const savedToDos = localStorage.getItem(TODOS_KEY) // string when console
 
 if (savedToDos) {
     const parsedToDos = JSON.parse(savedToDos)
+    toDos = parsedToDos // 여기 중요!!!
+    // localStorage data 가 있을 경우, 페이지를 새로 고침 하더라도 toDos 에 array 를 넣어줌
     parsedToDos.forEach(paintToDo) // newToDo 에 item 이 자동으로 들어가므로 언급할 필요 없음(!)
+    // cf: 배열이 7개가 넘어갈 경우? array 가 빈다 => 배열 개수가 아니라 새로고침 할때마다 array reset
 }
