0eb99dfe9fdefbbb7293fa1e741710da6aecd735
yaerhee <yaerhee@naver.com>
2022-10-14 23:55:57 +0900

    디자인 수정 - background, container div

--- web/js/background.js
+++ web/js/background.js
@@ -5,7 +5,9 @@ const images = [
 ]
 
 const chosenImage = images[Math.floor(Math.random() * images.length)]
-const backgroundImage = document.createElement('img')
+const backgroundDiv = document.querySelector('.background')
+const imgTag = document.createElement('img')
+const backgroundImage = backgroundDiv.appendChild(imgTag)
 backgroundImage.src = `img/${chosenImage}`
 
-document.body.appendChild(backgroundImage)
+// document.body.appendChild(backgroundImage)
